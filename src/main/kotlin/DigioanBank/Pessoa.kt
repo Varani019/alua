dd7f6f0673ecd03864e316ed431f62263a89f5af
Varani019 <89659785+Varani019@users.noreply.github.com>
2021-09-15 23:16:39 -0300

    primeiro commt

---
+++ src/main/kotlin/DigioanBank/Pessoa.kt
@@ -0,0 +1,15 @@
+package DigioanBank
+
+class Pessoa {
+    var nome: String = "luan"
+    var cpf: String = "123.122.111.17"
+
+}
+fun main(){
+
+ val luan= Pessoa()
+
+    println(luan.nome)
+    println(luan.cpf)
+
+}
