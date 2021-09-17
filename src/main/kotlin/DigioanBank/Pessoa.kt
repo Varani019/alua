1518042f5bd40ce09cb4970c2216890bd36e4cd0
Varani019 <89659785+Varani019@users.noreply.github.com>
2021-09-16 21:48:20 -0300

    coloquei o private

--- src/main/kotlin/DigioanBank/Pessoa.kt
+++ src/main/kotlin/DigioanBank/Pessoa.kt
@@ -2,12 +2,14 @@ package DigioanBank
 
 class Pessoa {
     var nome: String = "luan"
+
     var cpf: String = "123.122.111.17"
+    private set
 
 }
 fun main(){
+    val luan= Pessoa()
 
- val luan= Pessoa()
 
     println(luan.nome)
     println(luan.cpf)
