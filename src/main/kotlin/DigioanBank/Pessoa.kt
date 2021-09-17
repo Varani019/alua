b1a30c210e96dcafa3a5cfa6a33619482d47f6a9
Varani019 <89659785+Varani019@users.noreply.github.com>
2021-09-16 22:44:40 -0300

    conta

--- src/main/kotlin/DigioanBank/Pessoa.kt
+++ src/main/kotlin/DigioanBank/Pessoa.kt
@@ -3,15 +3,18 @@ package DigioanBank
 class Pessoa {
     var nome: String = "luan"
 
-    var cpf: String = "123.122.111.17"
+    var cpf: String = "123.122.113.17"
     private set
+
+    constructor()
+
+    fun nomeecpf() = "$nome + $cpf"
 
 }
 fun main(){
     val luan= Pessoa()
 
 
-    println(luan.nome)
-    println(luan.cpf)
+    println(luan.nomeecpf())
 
 }
