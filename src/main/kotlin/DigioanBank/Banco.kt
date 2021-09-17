09b7751d05961b07ecc94278ef95b611ee76e030
Varani019 <89659785+Varani019@users.noreply.github.com>
2021-09-17 13:53:02 -0300

    add banco

---
+++ src/main/kotlin/DigioanBank/Banco.kt
@@ -0,0 +1,6 @@
+package DigioanBank
+
+data class Banco(
+    val nome: String,
+    val numero: Int,
+)
---
+++ src/main/kotlin/Teste/BancoTeste.kt
@@ -0,0 +1,11 @@
+package Teste
+
+import DigioanBank.Banco
+
+fun main() {
+    val digionaBank = Banco(nome =  "luan", numero = 100)
+
+    println(digionaBank.nome)
+    println(digionaBank.numero)
+
+}
